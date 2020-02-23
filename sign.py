2018bcb9afac02aeb6bf69f9f3f6de194cf39467
7413karan <hp@DESKTOP-6RJ1DQD>
2020-02-23 23:38:07 +0530

    first commit

---
+++ sign.py
@@ -0,0 +1,21 @@
+import signup
+from tkinter import *
+o=Tk()
+l6=Label(text='Email')
+l6.grid(row=0,column=0)
+e5=Entry()
+e5.grid(row=0,column=1)
+l7=Label(text='Password')
+l7.grid(row=1,column=0)
+e6=Entry()
+e6.grid(row=1,column=1)
+
+        
+
+
+
+        
+
+b1 = Button(text='Login')
+b1.grid(row=2,column=1)       
+o.mainloop()
